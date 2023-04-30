f8b08943c7dd28b8dba94241012c9062f32f60ce
Bekzat <b_tursyn@kbtu.kz>
2023-04-30 12:09:35 +0600

    lesson3

---
+++ News/app/src/main/java/com/example/news/utils/Constants.kt
@@ -0,0 +1,8 @@
+package com.example.news.utils
+
+class Constants {
+    companion object {
+        const val BASE_URL = "https://newsapi.org/"
+        const val API_KEY = "434901d7116b4697a51079d955ea9691"
+    }
+}
