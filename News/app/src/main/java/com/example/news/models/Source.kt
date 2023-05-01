4b9bd193f1edfa9f8570e683776c95bd4044246a
Bekzat <b_tursyn@kbtu.kz>
2023-05-01 16:42:36 +0600

    BookStore

--- News/app/src/main/java/com/example/news/models/Source.kt
+++
@@ -1,6 +0,0 @@
-package com.example.news.models
-
-data class Source(
-    val id: String,
-    val name: String
-)
--- News/app/src/main/java/com/example/news/utils/Constants.kt
+++
@@ -1,8 +0,0 @@
-package com.example.news.utils
-
-class Constants {
-    companion object {
-        const val BASE_URL = "https://newsapi.org/"
-        const val API_KEY = "434901d7116b4697a51079d955ea9691"
-    }
-}
